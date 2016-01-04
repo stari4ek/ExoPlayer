6be672ccde1da1f2f46439adf94646a951428348
Michael Sotnikov <stari4ek@gmail.com>
2016-01-04 12:49:28 +0300

    update MulticatRendererBuilder to match support "Expose control over decoder selection"

--- demo/src/main/java/com/google/android/exoplayer/demo/player/MulticastRendererBuilder.java
+++ demo/src/main/java/com/google/android/exoplayer/demo/player/MulticastRendererBuilder.java
@@ -4,12 +4,14 @@
 package com.google.android.exoplayer.demo.player;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaCodec;
 import android.net.Uri;
 import android.text.TextUtils;
 import android.util.Log;
 
 import com.google.android.exoplayer.MediaCodecAudioTrackRenderer;
+import com.google.android.exoplayer.MediaCodecSelector;
 import com.google.android.exoplayer.MediaCodecVideoTrackRenderer;
 import com.google.android.exoplayer.TrackRenderer;
 import com.google.android.exoplayer.audio.AudioCapabilities;
@@ -61,10 +63,11 @@ public class MulticastRendererBuilder implements RendererBuilder {
     ExtractorSampleSource sampleSource = new ExtractorSampleSource(adoptedUri, dataSource, allocator,
         BUFFER_SEGMENT_COUNT * BUFFER_SEGMENT_SIZE, tsExtractor);
     MediaCodecVideoTrackRenderer videoRenderer = new MediaCodecVideoTrackRenderer(context,
-        sampleSource, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT, 5000, player.getMainHandler(),
-        player, 50);
+        sampleSource, MediaCodecSelector.DEFAULT, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT, 5000,
+        player.getMainHandler(), player, 50);
     MediaCodecAudioTrackRenderer audioRenderer = new MediaCodecAudioTrackRenderer(sampleSource,
-        null, true, player.getMainHandler(), player, AudioCapabilities.getCapabilities(context));
+        MediaCodecSelector.DEFAULT, null, true, player.getMainHandler(), player,
+        AudioCapabilities.getCapabilities(context), AudioManager.STREAM_MUSIC);
     TrackRenderer textRenderer = new TextTrackRenderer(sampleSource, player,
         player.getMainHandler().getLooper());
 
