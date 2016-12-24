176def3c080b10b7aaf577e37d0d8ed83c69be06
Michael Sotnikov <stari4ek@gmail.com>
2016-12-24 17:12:10 +0300

    New: Enable workarounds for MPEG-TS
    (cherry picked from commit 92962f7)

--- demo/src/main/java/com/google/android/exoplayer2/demo/PlayerActivity.java
+++ demo/src/main/java/com/google/android/exoplayer2/demo/PlayerActivity.java
@@ -105,14 +105,16 @@ public class PlayerActivity extends Activity implements OnClickListener, ExoPlay
     DEFAULT_COOKIE_MANAGER.setCookiePolicy(CookiePolicy.ACCEPT_ORIGINAL_SERVER);
   }
 
-  private Handler mainHandler;
-  private EventLogger eventLogger;
+  // TVirl: make it package
+  /* private */ Handler mainHandler;
+  /* private */ EventLogger eventLogger;
   private SimpleExoPlayerView simpleExoPlayerView;
   private LinearLayout debugRootView;
   private TextView debugTextView;
   private Button retryButton;
 
-  private DataSource.Factory mediaDataSourceFactory;
+  // TVirl: make it package
+  /* private */ DataSource.Factory mediaDataSourceFactory;
   private SimpleExoPlayer player;
   private DefaultTrackSelector trackSelector;
   private TrackSelectionHelper trackSelectionHelper;
@@ -344,7 +346,8 @@ public class PlayerActivity extends Activity implements OnClickListener, ExoPlay
     }
   }
 
-  private MediaSource buildMediaSource(Uri uri, String overrideExtension) {
+  // TVirl: make it package
+  /* private */  MediaSource buildMediaSource(Uri uri, String overrideExtension) {
     int type = TextUtils.isEmpty(overrideExtension) ? Util.inferContentType(uri)
         : Util.inferContentType("." + overrideExtension);
     switch (type) {
