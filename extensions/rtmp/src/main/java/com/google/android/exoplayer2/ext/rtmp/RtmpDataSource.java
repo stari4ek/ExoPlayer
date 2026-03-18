19244ea0da39024de3781a779fb9e9b8e4781998
stari4ek <michael.sotnikov@mapbox.com>
2026-03-18 23:07:11 +0100

    move to rtmp-client 3.2.0 from jitpack.io

--- extensions/rtmp/src/main/java/com/google/android/exoplayer2/ext/rtmp/RtmpDataSource.java
+++ extensions/rtmp/src/main/java/com/google/android/exoplayer2/ext/rtmp/RtmpDataSource.java
@@ -25,8 +25,9 @@ import com.google.android.exoplayer2.upstream.BaseDataSource;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DataSpec;
 import java.io.IOException;
-import net.butterflytv.rtmp_client.RtmpClient;
-import net.butterflytv.rtmp_client.RtmpClient.RtmpIOException;
+// TVirl: moved to 3.2.0 which also changes package name
+import io.antmedia.rtmp_client.RtmpClient;
+import io.antmedia.rtmp_client.RtmpClient.RtmpIOException;
 
 /** A Real-Time Messaging Protocol (RTMP) {@link DataSource}. */
 public final class RtmpDataSource extends BaseDataSource {
